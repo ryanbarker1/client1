bb76d023a0d6b68d9c5557edb8f22457cc7b2e22
ryanbarker1 <31492268+ryanbarker1@users.noreply.github.com>
2018-04-03 22:11:03 -0600

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
-    <hr>
+    <div class="text-center hr">
+      <br>
+    <img class="text-center" src="../assets/photos/mountain.png" width="250" alt="">
+    </div>
+
     <footer class=" container-fluid text-center">
       <div class="container">
         <div class="row">
           <nav class="navbar text-center">
             <div class="container-fluid">
               <ul class="nav navbar-nav">
-                <li> <div class="col-sm-8">
+                <li> <div class="col-sm-9">
                   <p>For information regarding this website, contact
                     <a href="mailto:webmaster@sunridge.info">the Webmaster</a></p>
                 </div></li>
-                <li>   <div class="col-sm-4">
-                  <button type="button" class="btn btn-info text-white"><a class="footer"  href="https://www.surveymonkey.com/r/KGSD262">Take Our Survey!</a></button>
-                </div></li>
-                <li class="active"><div class="col-sm-4">
+                <li  class="active"><div class="col-sm-3">
                   <a  href="https://www.facebook.com/SunridgePropertyOwnersAssociation?hc_location=stream">
-                    <button type="button" class="btn btn-primary"><i class="fa fa-facebook-square" style="font-size:16px"></i> Find us on Facebook</button></a>
+                   <i class="fab fa-facebook-square fa-3x face"></i></a>
                 </div></li>
               </ul>
             </div>
@@ -28,9 +29,21 @@
 </template>
 
 <style>
-  .footer{
-    color: white;
-  }
 
+  footer{
+  padding-top: 25px;
+    margin-top: 25px;
+/*background-color: black;*/
+
+  }
+.hr{
+  margin-top: 100px;
+}
+  .face{
+    color: #1094b2;
+  }
+.face:hover{
+  color: #072d6b;
+}
 
 </style>
